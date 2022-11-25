4b16c20d704ee36932678a8479e04df3a9287918
Lucas Cabral <lucascabral6008@gmail.com>
2022-11-24 21:08:14 -0300

    adding persist on refresh

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {AnimatePresence, AnmeatePresence} from "framer-motion";
 
 function App() {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter >
       <div className="w-screen h-auto flex flex-col bg-primary ">
         <Header />
         <main className="mt-24 p-8 w-full">
